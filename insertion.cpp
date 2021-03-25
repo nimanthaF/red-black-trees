6c976e54a08926b49435a7ab733a60dd3c7bee24
nimanthaF <nimanthafernando95@gmail.com>
2021-03-25 09:45:20 +0530

    adding rotations

--- insertion.cpp
+++ insertion.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Node {
 public:
 	int data;
-	Node* parent;
+	Node* p;
 	Node* left;
 	Node* right;
 	int color;           // 1- red and 0- black
@@ -16,33 +16,35 @@ typedef Node* NodePtr;
 class RBTree {
 public:
 	NodePtr root;
-	NodePtr leaf;
+	NodePtr TNULL;
 
 	RBTree() {
-		leaf = new Node;
-		leaf->color = 0;
-		leaf->left = nullptr;
-		leaf->right = nullptr;
+		TNULL = new Node;
+		TNULL->color = 0;
+		TNULL->left = nullptr;
+		TNULL->right = nullptr;
 		root = NULL;
 	}
 
 	void insert(int key);
 	void RBInsert(NodePtr k);
+	void leftRotate(NodePtr x);
+	void rightRotate(NodePtr x);
 };
 
 void RBTree::insert(int key) {
 	NodePtr node = new Node;
-	node->parent = nullptr;
+	node->p = nullptr;
 	node->data = key;
-	node->left = leaf;
-	node->right = leaf;
+	node->left = TNULL;
+	node->right = TNULL;
 	node->color = 1;
 
 
 	NodePtr y = nullptr;
 	NodePtr x = this->root;
 
-	while (x!=leaf) {
+	while (x!=TNULL) {
 		y = x;
 		if (node->data < x->data) {
 			x = x->left;
@@ -52,7 +54,7 @@ void RBTree::insert(int key) {
 		}
 	}
 
-	node->parent = y;
+	node->p = y;
 	if (y == nullptr) {
 		root = node;
 	}
@@ -64,12 +66,12 @@ void RBTree::insert(int key) {
 	}
 
 	//if new node is the root node return 
-	if (node->parent == nullptr) {
+	if (node->p == nullptr) {
 		node->color = 0;
 		return;
 	}
 
-	if (node->parent->parent == nullptr) {
+	if (node->p->p == nullptr) {
 		return;
 	}
 	RBInsert(node);
@@ -77,6 +79,52 @@ void RBTree::insert(int key) {
 
 void RBTree::RBInsert(NodePtr k) {
 
+}
+
+void RBTree::leftRotate(NodePtr x) {
+	NodePtr y = x->right;            //set y
+	x->right = y->left;               //turn y's left subtree into x's right subtree
+
+	if (y->left != TNULL) {
+		y->left->p = x;
+	}
+
+	y->p = x->p;
+
+	if (x->p == TNULL) {
+		this->root = y;
+	}
+	else if (x == x->p->left) {
+		x->p->left = y;
+	}
+	else {
+		x->p->right = y;
+	}
+	y->left = x;              //put x on y's left                                                       
+	x->p = y;
+}
+
+void RBTree::rightRotate(NodePtr x) {
+	NodePtr y = x->p;
+	y->left = x->right;
+
+	if (x->right != TNULL) {
+		x->right->p = y;
+	}
+
+	x->p = y->p;
+
+	if (y->p == TNULL) {
+		this->root = x;
+	}
+	else if (y == y->p->left) {
+		y->p->left = x;
+	}
+	else {
+		y->p->right = x;
+	}
+	x->right = y;
+	y->p = x;
 }
 
 int main()
